bf1ea6a9200d102be539ca050cd43ea6dc16811a
dnlespinosa <89487129+dnlespinosa@users.noreply.github.com>
2022-03-02 17:56:11 -0700

    Add files via upload

--- jeopardy.js
+++ jeopardy.js
@@ -19,7 +19,7 @@
 //  ]
 
 let categories = [];
-
+let catId = [];
 
 /** Get NUM_CATEGORIES random category from API.
  *
@@ -35,7 +35,8 @@ async function getCategoryIds() {
         // console.log(id.category.id);
     }
     const randInt = Math.floor(Math.random()*100)+1;
-    return arr[randInt];
+    catId.push(arr[randInt]);
+    return catId;
     }
 
 /** Return object with data about a category:
@@ -69,7 +70,8 @@ async function getCategory(catId) {
         }
     }
 
-    return arr[0];
+    categories.push(arr[0]);
+    return categories;
 }
 
 /** Fill the HTML table#jeopardy with the categories & cells for questions.
@@ -80,18 +82,18 @@ async function getCategory(catId) {
  *   (initally, just show a "?" where the question/answer would go.)
  */
 
-async function fillTable() {
-    const body = document.querySelector('body');
 
+
+async function fillTable() {
+
+    const body = document.querySelector('body');
     const board = document.createElement('table');
     board.setAttribute('id', 'board');
-
     const top = document.createElement('thead');
     top.setAttribute('id', 'top-head');
     for (let x =0; x<6; x++){
-
         const headCell = document.createElement('td');
-        headCell.setAttribute('id', 'x');
+        headCell.setAttribute('id', `${x}`);
         top.append(headCell);
     }
     board.append(top);
@@ -112,7 +114,44 @@ async function fillTable() {
 
 }
 
-fillTable();
+
+
+function fillCatId() {
+    for (let x=0; x<6; x++){
+        getCategoryIds();
+    }
+}
+
+function fillCategories() {
+    for (let x=0; x<6; x++){
+        getCategory(catId[x]);
+    }
+}
+
+function insertHeaders() {
+    for (let x=0; x<6; x++){
+        let item = document.getElementById(`${x}`);
+        if (categories[x] === undefined){
+            item.innerText = 'RELOAD PAGE';
+        } else {
+            item.innerText = categories[x].title
+        }
+    }
+    for (let x=0; x<6; x++){
+        for (let y=0; y<6; y++){
+            let itemBlock = document.getElementById(`${y}-${x}`);
+            if (categories[x].clues[y].question === ''){
+                itemBlock.innerText = 'RELOAD PAGE';
+            } else {
+                itemBlock.innerText = categories[x].clues[y].question
+            }
+        }
+            
+    }
+}
+
+
+
 
 /** Handle clicking on a clue: show the question or answer.
  *
@@ -155,3 +194,7 @@ async function setupAndStart() {
 /** On page load, add event handler for clicking clues */
 
 // TODO
+
+
+
+fillTable();
